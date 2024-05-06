acb27a2db1e02a9f1f6a9981f2a1b398787cd578
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-05-06 01:49:12 -0400

    updated html routing file

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,6 @@
+const app = require('express').Router();
+const path = require('path');
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+module.exports = app;
