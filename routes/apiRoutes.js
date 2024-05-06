93e4d5e3fadbd55bbad9b19cfdf7b3448f6e4813
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-05-06 01:46:11 -0400

    set up routing for api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,19 @@
 const app = require('express').Router();
 const updater = require('../public/assets/js/update');
 
-// 
+// take the created class of update and convert to json
+app.get('/notes', (req,res) => {
+    updater.addNote(req.body)
+    .then((note) => {
+        res.json(note)
+    })
+})
+
+// removes note with given id and re-updates the list
+app.delete('/notes/:id', (req, res) => {
+    updater.removeNote(req.params.id)
+    .then((note) => {
+        res.json(note)
+    })
+})
+module.exports = app;
